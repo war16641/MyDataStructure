a1968d2be3407246bd159f3dde22fd56a13ce8db
war16641 <44914209+war16641@users.noreply.github.com>
2020-12-06 23:03:08 +0800

    第一周

--- MyDataStructure/MyDataStructure.cs
+++ MyDataStructure/MyDataStructure.cs
@@ -1,12 +1,80 @@
 ﻿using System;
-using System;
+
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Office.Interop.Excel;
 using Excel = Microsoft.Office.Interop.Excel;
 namespace MyDataStructure
 {
-    public class DataUnit
+
+    public static class MyStatistic
+    {
+        /// <summary>
+        /// 求和
+        /// 如果这个object没有+，会抛出错误
+        /// </summary>
+        /// <param name="lst"></param>
+        /// <returns></returns>
+        public static object sum(List<object> lst)
+        {
+            object rt = null;
+            foreach (var item in lst)
+            {
+                if (null == rt)
+                {
+                    rt = item;
+                }
+                else
+                {
+                    rt = (dynamic)rt + (dynamic)item;
+                }
+            }
+            return rt;
+        }
+
+        public static int count(List<object> lst)
+        {
+            return lst.Count;
+        }
+
+        /// <summary>
+        /// 求平均
+        /// 要求实现+  ， 与数相乘 否则抛出错误
+        /// </summary>
+        /// <param name="lst"></param>
+        /// <returns></returns>
+        public static object avg(List<object> lst)
+        {
+            object rt = MyStatistic.sum(lst);
+            return (dynamic)rt * (1.0 / (double)lst.Count);
+        }
+
+        public static object max(List<object> lst)
+        {
+            object cur = lst[0];
+            for (int i = 1; i < lst.Count; i++)
+            {
+                if (((IComparable)cur).CompareTo(lst[i]) == -1)
+                {
+                    cur = lst[i];
+                }
+            }
+            return cur;
+        }
+        public static object min(List<object> lst)
+        {
+            object cur = lst[0];
+            for (int i = 1; i < lst.Count; i++)
+            {
+                if (((IComparable)cur).CompareTo(lst[i]) == 1)
+                {
+                    cur = lst[i];
+                }
+            }
+            return cur;
+        }
+    }
+    public class DataUnit:ICloneable
     {
         public Dictionary<string, object> data = new Dictionary<string, object>();
         public FlatDataModel fdm;
@@ -24,9 +92,33 @@ namespace MyDataStructure
             }
             return s;
         }
+
+
+        /// <summary>
+        /// 浅拷贝
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            DataUnit du = new DataUnit(this.fdm);
+            foreach (string key in this.data.Keys)
+            {
+                du.data.Add(key, this.data[key]);
+            }
+            return du;
+        }
     }
 
-    public class FlatDataModel : IEnumerable
+
+    public class FlatDataModelException: ApplicationException
+    {
+
+        public FlatDataModelException(string s) : base(s)
+        {
+            
+        }
+    }
+    public class FlatDataModel : IEnumerable,ICloneable
     {
         public List<DataUnit> units = new List<DataUnit>();
         public List<string> vn = new List<string>();//字段名
@@ -165,5 +257,311 @@ namespace MyDataStructure
             }
 
         }
+
+
+        /// <summary>
+        /// 找到一个复合条件的u
+        /// 没找到 返回null
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public DataUnit find_one(System.Func<DataUnit,bool> func,out int index)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (func(this.units[i]))
+                {
+                    index = i;
+                    return this.units[i];
+                }
+            }
+            index = -1;
+            return null;
+        }
+
+        /// <summary>
+        /// 找到一个复合条件的u
+        /// 没找到 返回null
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public DataUnit find_one(System.Func<DataUnit, bool> func)
+        {
+            int _i;
+            return this.find_one(func, out _i);
+        }
+
+
+        public void sort(System.Func<DataUnit, DataUnit,int> func)
+        {
+            this.units.Sort((x,y)=>func(x,y));
+
+        }
+
+        public void sort(List<string> keys)
+        {
+            foreach (string item in keys)
+            {
+                if (!this.vn.Contains(item))
+                {
+                    throw new System.ArgumentException("关键字段不在字段名中。");
+                }
+            }
+
+            this.units.Sort(delegate (DataUnit a, DataUnit b)
+            {
+                int t;
+                foreach (string key in keys)
+                {
+                    if(!(a.data[key] is IComparable))
+                    {
+                        throw new FlatDataModelException(string.Format("字段{0}未实现Icomparable"));
+                    }
+                    IComparable ic = (IComparable)a.data[key];
+                    t = ic.CompareTo(b.data[key]);
+                    if (t != 0)
+                    {
+                        return t;
+                    }
+                    //比平了，比下一个
+                }
+                //如果所有字段都比平了
+                return 1;
+            });
+        }
+
+        public List<List<DataUnit>>  make_bunch(List<string> classifyname)
+        {
+            foreach (string item in classifyname)
+            {
+                if(!this.vn.Contains(item))
+                {
+                    throw new System.ArgumentException("分类字段不在字段名中。");
+                }
+            }
+
+            List<List<DataUnit>> rt = new List<List<DataUnit>>();
+            //先排序
+            this.sort(classifyname);
+
+            //判断是否是同一个bunch
+            System.Func<DataUnit, DataUnit, bool> is_same_bunch = delegate(DataUnit a,DataUnit b) {
+                foreach (string n in classifyname)
+                {
+
+                    //if (a.data[n]!=b.data[n])
+                    //{
+                    //    return false;
+                    //}
+
+                    dynamic a1 = a.data[n];
+                    dynamic b1 = b.data[n];
+                    if (a1 != b1) return false;
+                }
+                return true;
+            };
+
+
+            List<DataUnit> cur_bunch = new List<DataUnit>();
+            DataUnit cur_bunch_name = this.units[0];//当前bunch
+            foreach (DataUnit item in this)
+            {
+                if (is_same_bunch(item,cur_bunch_name))
+                {
+                    cur_bunch.Add(item);
+                }
+                else
+                {
+                    //当前bunch结束
+                    rt.Add(cur_bunch);
+                    cur_bunch_name = item;
+                    cur_bunch = new List<DataUnit>();
+                    cur_bunch.Add(item);
+                }
+            }
+            if (cur_bunch.Count>0)
+            {
+                rt.Add(cur_bunch);
+            }
+
+            return rt;
+        }
+
+
+        /// <summary>
+        /// 浅拷贝
+        /// 如果是字符串和数 拷贝后与源完全独立
+        /// 如果是对象 就是互连的
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            FlatDataModel fdm = new FlatDataModel();
+            this.vn.ForEach(i => fdm.vn.Add(i));//复制vn
+            DataUnit u;
+            foreach (DataUnit item in this)
+            {
+                u = (DataUnit)item.Clone();
+                u.fdm = fdm;
+                fdm.units.Add(u);
+            }
+            
+            return fdm;
+        }
+
+        public FlatDataModel flhz(List<string> classifynames,params FLHZ_OPERATION[] par)
+        {
+            FlatDataModel t_fdm = (FlatDataModel)this.Clone();
+            List<List<DataUnit>> bunches = t_fdm.make_bunch(classifynames);
+            foreach (FLHZ_OPERATION item in par)
+            {
+                //先检查fieldname是否合法
+                if (!t_fdm.vn.Contains(item.fieldname))
+                {
+                    throw new ArgumentException(string.Format("不存在字段名{0}", item.fieldname));
+
+                }
+
+                //处理newname的默认值
+                if (item.newname.Length == 0) item.newname = item.fieldname;
+
+            }
+
+            //开始统计
+            FlatDataModel rt = new FlatDataModel();
+            
+            foreach (List<DataUnit> thisbunch in bunches)
+            {
+                //先写入classfynames
+                DataUnit du = new DataUnit(rt);
+                foreach (string n in classifynames)
+                {
+                    du.data.Add(n, thisbunch[0].data[n]);
+                }
+
+
+                //再写入统计数据
+                foreach (FLHZ_OPERATION op in par)
+                {
+                    //把List<DataUnit>中fieldname提取出来
+                    List<object> lo = new List<object>();
+                    foreach (DataUnit u in thisbunch)
+                    {
+                        lo.Add(u.data[op.fieldname]);
+                    }
+                    //统计
+                    du.data.Add(op.newname, op.func(lo));
+                }
+                rt.units.Add(du);
+            }
+
+            //整理vn
+            foreach (string item in classifynames)
+            {
+                rt.vn.Add(item);
+            }
+            foreach (FLHZ_OPERATION item in par)
+            {
+                rt.vn.Add(item.newname);
+            }
+            return rt;
+        }
+
+        /// <summary>
+        /// 提取一列数据出来
+        /// </summary>
+        /// <param name="fieldname"></param>
+        /// <returns></returns>
+        public List<object> get_col(string fieldname)
+        {
+            //先检查fieldname是否合法
+            if (!this.vn.Contains(fieldname))
+            {
+                throw new ArgumentException(string.Format("不存在字段名{0}", fieldname));
+
+            }
+
+            List<object> rt = new List<object>();
+            foreach (DataUnit item in this)
+            {
+                rt.Add(item.data[fieldname]);
+            }
+            return rt;
+        }
+
+
+        /// <summary>
+        /// 从其他模型中添加字段
+        /// </summary>
+        /// <param name="link_field">连接字段</param>
+        /// <param name="add_fields">添加字段</param>
+        /// <param name="other_model">其他模型</param>
+        /// <param name="default_value">默认值，在找不到对应dataunit的时候使用</param>
+        public void add_field_from_other_model(string link_field,string[] add_fields,FlatDataModel other_model,object default_value)
+        {
+            //检查字段名的合法性
+            if(!this.vn.Contains(link_field) ||!other_model.vn.Contains(link_field))
+            {
+                throw new ArgumentException(string.Format("字段名{0}不存在。", link_field));
+            }
+            foreach (var item in add_fields)
+            {
+                if(!other_model.vn.Contains(item))
+                {
+                    throw new ArgumentException(string.Format("字段名{0}不存在。", item));
+                }
+                if(this.vn.Contains(item))
+                {
+                    throw new ArgumentException(string.Format("字段名{0}已经存在。", item));
+                }
+            }
+
+            //开始连接
+            foreach (DataUnit u in this)
+            {
+                var umatch = other_model.find_one(delegate (DataUnit tu)
+                  {
+                      dynamic d1 = tu.data[link_field];
+                      dynamic d2 = u.data[link_field];
+                      return d1 == d2;
+                  });
+                if(null==umatch)
+                {
+                    //没找到
+                    foreach (string item in add_fields)
+                    {
+                        u.data.Add(item, default_value);
+                    }
+                }
+                else//找到
+                {
+                    foreach (string item in add_fields)
+                    {
+                        u.data.Add(item, umatch.data[item]);
+                    }
+                }
+            }
+
+            //更新vn
+            foreach (string item in add_fields)
+            {
+                this.vn.Add(item);
+            }
+        }
     }
+
+    /// <summary>
+    /// 用于flhz的参数
+    /// </summary>
+    public class FLHZ_OPERATION
+    {
+        public string fieldname = "";//原字段名 也是被统计的字段
+        public string newname = "";//新 字段名 空时=原字段名
+        public Func<List<object>, object> func;//统计函数
+        
+
+    }
+
 }
--- MyDataStructureTests/FlatDataModelTests.cs
+++ MyDataStructureTests/FlatDataModelTests.cs
@@ -14,13 +14,114 @@ namespace MyDataStructure.Tests
         [TestMethod()]
         public void load_from_excelTest()
         {
-            FlatDataModel fdm = FlatDataModel.load_from_excel(Environment.CurrentDirectory+"\\测试需要\\工作簿1.xlsx");
-            string str2 = Environment.CurrentDirectory;//获取和设置当前目录（即该进程从中启动的目录）的完全限定路径
-            System.Diagnostics.Debug.Print(str2);
-            System.Diagnostics.Debug.Print(System.IO.Directory.GetCurrentDirectory());
-            Assert.Fail();
+            FlatDataModel fdm = FlatDataModel.load_from_excel(Environment.CurrentDirectory + "\\测试需要\\工作簿1.xlsx");
+            //string str2 = Environment.CurrentDirectory;//获取和设置当前目录（即该进程从中启动的目录）的完全限定路径
+            //System.Diagnostics.Debug.Print(str2);
+            //System.Diagnostics.Debug.Print(System.IO.Directory.GetCurrentDirectory());
+            Assert.AreEqual(300.0, (double)fdm.units[1].data["年龄"], 0.001);
+
         }
 
+        [TestMethod()]
+        public void find_oneTest()
+        {
+            FlatDataModel fdm = FlatDataModel.load_from_excel(Environment.CurrentDirectory + "\\测试需要\\工作簿1.xlsx");
+            int iint;
+            DataUnit u = fdm.find_one(delegate (DataUnit u)
+            {
+                if ((double)u.data["年龄"] == 20.0)
+                {
+                    return true;
+                }
+                return false;
+            },
+            out iint);
+            Assert.AreEqual("爱丽丝", u.data["姓名"].ToString());
+            Assert.AreEqual(2, iint);
+        }
 
+        [TestMethod()]
+        public void sortTest()
+        {
+            FlatDataModel fdm = FlatDataModel.load_from_excel(
+                Environment.CurrentDirectory + "\\测试需要\\工作簿1.xlsx");
+            fdm.sort(delegate (DataUnit a, DataUnit b)
+            {
+                if ((double)a.data["年龄"] > (double)b.data["年龄"]) return 1;
+                return -1;
+            });
+            Assert.AreEqual("奥特曼", fdm.units[2].data["姓名"]);
+
+
+            fdm = FlatDataModel.load_from_excel(
+                Environment.CurrentDirectory + "\\测试需要\\flhz.xlsx");
+            List<string> lst = new List<string>();
+            lst.Add("身高");
+            lst.Add("年龄");
+            fdm.sort(lst);
+            //fdm.show_in_excel();
+            Assert.AreEqual("普京", fdm.units[fdm.Count - 2].data["姓名"]);
+
+        }
+
+        //[TestMethod()]
+        //public void make_bunchTest()
+        //{
+        //    FlatDataModel fdm = FlatDataModel.load_from_excel(
+        //        Environment.CurrentDirectory + "\\测试需要\\flhz.xlsx");
+        //    string a = "sd";
+        //    double s = 12.2;
+        //}
+
+        [TestMethod()]
+        public void avgTest()
+        {
+            List<object> lst = new List<object>();
+            lst.Add(1.0);
+            lst.Add(2.0);
+            lst.Add(3.0);
+            Assert.AreEqual(2.0, (double)MyStatistic.avg(lst));
+            Assert.AreEqual(1.0, (double)MyStatistic.min(lst));
+        }
+
+        [TestMethod()]
+        public void flhzTest()
+        {
+            FlatDataModel fdm = FlatDataModel.load_from_excel(
+                Environment.CurrentDirectory + "\\测试需要\\flhz.xlsx");
+
+            FLHZ_OPERATION f1 = new FLHZ_OPERATION();
+            f1.fieldname = "身高";
+            f1.func = MyStatistic.sum;
+            FLHZ_OPERATION f2 = new FLHZ_OPERATION();
+            f2.fieldname = "身高";
+            f2.newname = "平均身高";
+            f2.func = MyStatistic.avg;
+            FLHZ_OPERATION f3 = new FLHZ_OPERATION();
+            f3.fieldname = "宣言";
+            f3.func = MyStatistic.sum;
+            var lo = new List<string>() { "性别", "种族" };
+            FlatDataModel rt = fdm.flhz(lo, f1, f2, f3);
+            //rt.show_in_excel();
+            Assert.AreEqual(4, rt.Count);
+            Assert.AreEqual(174.0, rt.units[0].data["平均身高"]);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void add_field_from_other_modelTest()
+        {
+            FlatDataModel fdm = FlatDataModel.load_from_excel(
+                Environment.CurrentDirectory + "\\测试需要\\flhz.xlsx");
+            FlatDataModel fdmo = FlatDataModel.load_from_excel(
+                Environment.CurrentDirectory + "\\测试需要\\添加字段.xlsx");
+            string[] s = new string[] { "声望" ,"归属地"};
+            fdm.add_field_from_other_model("姓名",s, fdmo, "sd");
+            //fdm.show_in_excel();
+            
+            Assert.AreEqual(1000.0,(double)fdm.units[0].data["声望"],0.001);
+            Assert.AreEqual("美国", (string)fdm.units[2].data["归属地"]);
+            Assert.AreEqual("sd", (string)fdm.units[fdm.Count-1].data["归属地"]);
+        }
     }
 }
